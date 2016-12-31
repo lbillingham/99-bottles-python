c548bf7c3d98ba0526f0f5523d9fe74efa9f3c5b
Laurence Billingham <laurence.billingham@gmail.com>
2016-12-31 00:50:55 +0000

    1st verse test passes in ruby

---
+++ ruby/lib/bottles.rb
@@ -0,0 +1,9 @@
+class Bottles
+    def verse(arg)
+    out_verse = <<-VERSE
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+VERSE
+    out_verse
+    end
+end
