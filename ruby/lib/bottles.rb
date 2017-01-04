9d7808e1d776a4efe065b364446da0593742a0cb
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-04 21:51:16 +0000

    ruby clean in master for begining anew

--- ruby/lib/bottles.rb
+++ ruby/lib/bottles.rb
@@ -1,9 +1,2 @@
 class Bottles
-    def verse(arg)
-    out_verse = <<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
-VERSE
-    out_verse
-    end
 end
