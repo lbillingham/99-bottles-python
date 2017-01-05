9cc07874559a69b01a38db8d9d730d59ddd68e0c
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-05 22:44:09 +0000

    1st passing test in ruby: 99 bottles verse

--- ruby/test/bottles_test.rb
+++ ruby/test/bottles_test.rb
@@ -3,4 +3,11 @@ require 'minitest/autorun'
 require_relative '../lib/bottles'
 
 class BottlesTest < Minitest::Test
+    def test_99bottles_verse
+        expected = "99 bottles of beer on the wall, " +
+                   "99 bottles of beer.\n" +
+                   "Take one down and pass it around, " +
+                   "98 bottles of beer on the wall.\n"
+        assert_equal expected, Bottles.new.verse(99)
+    end
 end
