9c195c20098ffd608bb234ad7ebf8b0524db4270
Billingham <laurence@bgs.ac.uk>
2017-01-05 22:59:08 +0000

    1st passing python test, 99bottles verse

--- python/bottles/bottles.py
+++ python/bottles/bottles.py
@@ -1,5 +1,12 @@
 """
 code to 'sing' 99 bottles of beer, after the Metz and Owen
 OOP design book
-
 """
+
+def verse(_):
+    """'sing' one single verse of the song"""
+    lyrics = ("99 bottles of beer on the wall, " +
+              "99 bottles of beer.\n" +
+              "Take one down and pass it around, " +
+              "98 bottles of beer on the wall.\n")
+    return lyrics
---
+++ python/test/bottles_tests.py
@@ -0,0 +1,19 @@
+"""
+tests for our code that is supposed to 'sing'
+the 99 bottles of beer song
+after the Metz and Owen
+OOP design book
+but in python
+"""
+from bottles import bottles
+
+def test_99_bottles_verse():
+    """
+    can we 'sing' the verse beginning
+    '99 bottles of beer on the wall, 99 bottles of beer ...'
+    """
+    expected = ("99 bottles of beer on the wall, " +
+                "99 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                "98 bottles of beer on the wall.\n")
+    assert expected == bottles.verse(99)
