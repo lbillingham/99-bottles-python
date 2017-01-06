84e2c01f938735a66d513b85ce9be29dcc9a7191
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-06 21:47:37 +0000

    doh now actually with the test in ruby

--- ruby/test/bottles_test.rb
+++ ruby/test/bottles_test.rb
@@ -33,4 +33,12 @@ class BottlesTest < Minitest::Test
                 "No more bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(1)
     end
+
+    def test_0bottles_verse
+     expected = "No more bottles of beer on the wall, " +
+                "no more bottles of beer.\n" +
+                "Go to the store and buy some more, " +
+                "99 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(0)
+    end
 end
