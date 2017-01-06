d6b8a306c02d0eb59e225fd9710f65471ce94ed2
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-06 11:25:02 +0000

    doh, now without redundant conditionals in ruby

--- ruby/lib/bottles.rb
+++ ruby/lib/bottles.rb
@@ -1,10 +1,5 @@
 class Bottles
     def verse(number)
-        if number == 99
-            n = 99
-        else
-            n = 3
-        end
     "#{number} bottles of beer on the wall, " +
     "#{number} bottles of beer.\n" +
     "Take one down and pass it around, " +
