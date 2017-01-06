95a669ac0d47ea490f9788a4c709d343bb03e564
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-06 17:01:48 +0000

    verse2 with `pluralize` function....
    this abstaction seems shonky

--- python/bottles/bottles.py
+++ python/bottles/bottles.py
@@ -2,13 +2,20 @@
 code to 'sing' 99 bottles of beer, after the Metz and Owen
 OOP design book
 """
+def plural(num):
+    """do we have a `'foo'` or some `'foos'`"""
+    plurl = 's'
+    if num == 1:
+        plurl = ''
+    return plurl
+
 
 def verse(num_bottles):
     """'sing' one single verse of the song"""
     lyrics = (f"{num_bottles} bottles of beer on the wall, " +
               f"{num_bottles} bottles of beer.\n" +
               "Take one down and pass it around, " +
-              f"{num_bottles - 1} bottle{'' if num_bottles - 1 == 1 else 's'} " +
+              f"{num_bottles - 1} bottle{plural(num_bottles-1)} " +
               "of beer on the wall.\n")
 
     return lyrics
--- ruby/lib/bottles.rb
+++ ruby/lib/bottles.rb
@@ -1,9 +1,14 @@
 class Bottles
+
+    def pluralize(number)
+        "bottle#{'s' unless (number-1) == 1}"
+    end
+
     def verse(number)
     "#{number} bottles of beer on the wall, " +
     "#{number} bottles of beer.\n" +
     "Take one down and pass it around, " +
-    "#{number-1} bottle#{'s' unless number-1 == 1} of beer " +
+    "#{number-1} #{pluralize(number)} of beer " +
     "on the wall.\n"
     end
 end
