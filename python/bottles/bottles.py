04789fa7f7e67418b165256fb79fd403be55dd52
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-06 11:20:27 +0000

    full interpolation python and ruby

--- python/bottles/bottles.py
+++ python/bottles/bottles.py
@@ -5,14 +5,9 @@ OOP design book
 
 def verse(num_bottles):
     """'sing' one single verse of the song"""
-    if num_bottles == 99:
-        lyrics = ("99 bottles of beer on the wall, " +
-                  "99 bottles of beer.\n" +
-                  "Take one down and pass it around, " +
-                  "98 bottles of beer on the wall.\n")
-    else:
-        lyrics = ("3 bottles of beer on the wall, " +
-                  "3 bottles of beer.\n" +
-                  "Take one down and pass it around, " +
-                  "2 bottles of beer on the wall.\n")
+    lyrics = (f"{num_bottles} bottles of beer on the wall, " +
+              f"{num_bottles} bottles of beer.\n" +
+              "Take one down and pass it around, " +
+              f"{num_bottles - 1} bottles of beer on the wall.\n")
+
     return lyrics
