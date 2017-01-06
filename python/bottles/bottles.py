4ea36a0b3140542da7a388de6d2a0f0a2477c2dd
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-06 17:09:39 +0000

    verse2 test passes with stark conditional

--- python/bottles/bottles.py
+++ python/bottles/bottles.py
@@ -2,20 +2,19 @@
 code to 'sing' 99 bottles of beer, after the Metz and Owen
 OOP design book
 """
-def plural(num):
-    """do we have a `'foo'` or some `'foos'`"""
-    plurl = 's'
-    if num == 1:
-        plurl = ''
-    return plurl
 
 
 def verse(num_bottles):
     """'sing' one single verse of the song"""
-    lyrics = (f"{num_bottles} bottles of beer on the wall, " +
-              f"{num_bottles} bottles of beer.\n" +
-              "Take one down and pass it around, " +
-              f"{num_bottles - 1} bottle{plural(num_bottles-1)} " +
-              "of beer on the wall.\n")
+    if num_bottles == 2:
+        lyrics = ("2 bottles of beer on the wall, " +
+                  "2 bottles of beer.\n" +
+                  "Take one down and pass it around, " +
+                  "1 bottle of beer on the wall.\n")
+    else:
+        lyrics = (f"{num_bottles} bottles of beer on the wall, " +
+                  f"{num_bottles} bottles of beer.\n" +
+                  "Take one down and pass it around, " +
+                  f"{num_bottles - 1} bottles of beer on the wall.\n")
 
     return lyrics
