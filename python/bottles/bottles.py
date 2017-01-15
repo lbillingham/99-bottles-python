8d19f275e9c4f53bf215556105f22dcf3d0557c7
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-15 21:43:20 +0000

    six pack test and naive conditional code

--- python/bottles/bottles.py
+++ python/bottles/bottles.py
@@ -18,7 +18,15 @@ def verse(num_bottles):
     2: ("2 bottles of beer on the wall, " +
         "2 bottles of beer.\n" +
         "Take one down and pass it around, " +
-        "1 bottle of beer on the wall.\n")
+        "1 bottle of beer on the wall.\n"),
+    6: ("1 six-pack of beer on the wall, " +
+        "1 six-pack of beer.\n" +
+        "Take one down and pass it around, " +
+        "5 bottles of beer on the wall.\n"),
+    7: ("7 bottles of beer on the wall, " +
+        "7 bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "1 six-pack of beer on the wall.\n")
     }
     default = (f"{num_bottles} bottles of beer on the wall, " +
                f"{num_bottles} bottles of beer.\n" +
--- python/test/bottles_tests.py
+++ python/test/bottles_tests.py
@@ -89,6 +89,22 @@ def test_verses_99_to_98():
     assert expected == bottles.verses(99, 98)
 
 
+def test_verses_7_to_6_sixpack():
+    """
+    when we would otherwise 'sing' about 6 bottles,
+    we should use '1 six-pack' instead
+    """
+    expected = ("7 bottles of beer on the wall, " +
+                "7 bottles of beer.\n" +
+                "Take one down and pass it around, " +
+                "1 six-pack of beer on the wall.\n"
+                "\n"
+                "1 six-pack of beer on the wall, "
+                "1 six-pack of beer.\n" +
+                "Take one down and pass it around, " +
+                "5 bottles of beer on the wall.\n")
+    assert expected == bottles.verses(7, 6)
+
 def test_verses_2_to_0():
     """
     can we 'sing' verse
@@ -392,9 +408,9 @@ Take one down and pass it around, 8 bottles of beer on the wall.
 Take one down and pass it around, 7 bottles of beer on the wall.
 
 7 bottles of beer on the wall, 7 bottles of beer.
-Take one down and pass it around, 6 bottles of beer on the wall.
+Take one down and pass it around, 1 six-pack of beer on the wall.
 
-6 bottles of beer on the wall, 6 bottles of beer.
+1 six-pack of beer on the wall, 1 six-pack of beer.
 Take one down and pass it around, 5 bottles of beer on the wall.
 
 5 bottles of beer on the wall, 5 bottles of beer.
