e61d4803739b66a0163b5a0c8720ac1f6ee4bae1
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-15 22:41:59 +0000

    default and 2-bottles verse now same

--- ruby/lib/bottles.rb
+++ ruby/lib/bottles.rb
@@ -13,12 +13,12 @@ class Bottles
             "Take it down and pass it around, " +
             "no more bottles of beer on the wall.\n"
         when 2
-            "2 bottles of beer on the wall, " +
-            "2 bottles of beer.\n" +
+            "#{number} bottles of beer on the wall, " +
+            "#{number} bottles of beer.\n" +
             "Take one down and pass it around, " +
-            "1 bottle of beer on the wall.\n"
+            "#{number-1} #{container(number-1)} of beer on the wall.\n"
         else
-            "#{number} #{container} of beer on the wall, " +
+            "#{number} bottles of beer on the wall, " +
             "#{number} bottles of beer.\n" +
             "Take one down and pass it around, " +
             "#{number-1} #{container(number-1)} of beer on the wall.\n"
@@ -33,7 +33,7 @@ class Bottles
         verses(99, 0)
     end
 
-    def container(number=:FIXME)
+    def container(number)
         if number == 1
             "bottle"
         else
