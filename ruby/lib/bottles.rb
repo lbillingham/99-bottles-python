879ef4cd0d5a4d5d35cf58488b7238c3b68af0ff
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-15 22:38:15 +0000

    now pass arg to container

--- ruby/lib/bottles.rb
+++ ruby/lib/bottles.rb
@@ -21,7 +21,7 @@ class Bottles
             "#{number} #{container} of beer on the wall, " +
             "#{number} bottles of beer.\n" +
             "Take one down and pass it around, " +
-            "#{number-1} bottles of beer on the wall.\n"
+            "#{number-1} #{container(number-1)} of beer on the wall.\n"
         end
     end
 
